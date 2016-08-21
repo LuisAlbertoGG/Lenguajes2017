bccbb692acc9ccc8d1e208c8c70d17b6659d2a44
AlejandroVaGa <ale.valdes@ciencias.unam.mx>
2016-08-21 16:48:41 -0500

    Ejercicio 8

--- Práctica1/Práctica 1.rkt
+++ Práctica1/Práctica 1.rkt
@@ -87,12 +87,41 @@
     [(empty? l) '()]
     [(foldl cons '() l)]
     ))
-;8a no la terminé
-(define (concatena l m)
+
+;8a - Concatenación de dos listas
+(define (concatena l1 l2)
+  (cond ((empty? l1) l2)
+        ((empty? l2) l1)
+        (else (cons (car l1) (concatena (cdr l1) l2)))))
+
+;8b - Funcion a la lista
+(define (mapea f l)
   (cond
-    [(empty? m) m]
-    [else (concatena (cons l (list (car m))) (cdr m))]
-    ))
+    [(empty? l) l]
+    [else (cons (f (car l)) (mapea f (cdr l)))]))
+
+;8c - Filtra
+(define (filtra p l)
+  (cond
+    [(empty? l) l]
+    [(p (car l)) (cons (car l) (filtra p (cdr l)))]
+    [(filtra p (cdr l))]))
+
+;8d Primeros n elementos
+(define (toma l n)
+  (define (aux li i j)
+    (cond
+    [(zero? j) i]
+    [else (aux (cdr li) (append i (list (car li))) (sub1 j))]))
+  (aux l empty n))
+
+;8e - quita
+(define (quita l n)
+  (define (aux1 li i j)
+    (cond
+      [(zero? j) i]
+      [else (aux1 (toma li (add1 j)) (append i (list (last li))) (sub1 j))]))
+  (aux1 l empty n))
 
 ;9 a
 (letrec ([ mayor ( lambda ( n m )
