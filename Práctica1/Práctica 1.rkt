bf608167fc6a18604a861fb0f53238490d092e0a
Rodrigo Rivera <Rodrigo_Rivera@Rodrigo-2.local>
2016-09-13 13:02:23 -0500

    practica 2

---
+++ Práctica1/Práctica 1.rkt
@@ -0,0 +1,167 @@
+#lang plai
+
+;ejerccio #1
+
+(define (ec-lin a b)
+  display (/(- b )a))
+
+(test (ec-lin 3 2) (-(/ 2 3)))
+(test (ec-lin 5 5) -1)
+(test (ec-lin 4 3) (-(/ 3 4)))
+(test (ec-lin 3 4) (-(/ 4 3)))
+(test (ec-lin 2 5) (-(/ 5 2)))
+
+;ejerccio #2
+
+(define (area-heron a b c)
+  (let ([s (/(+ a b  c) 2)]
+        ) 
+    display (sqrt
+                  (* s (- s a) (- s b )(- s c)))
+    )
+  )
+(test (area-heron 3 25 26) 36)
+(test (area-heron 5 7 10) 16.24)
+(test (area-heron 10 15 20) 72.61)
+(test (area-heron 5 25 26) 62.16)
+(test (area-heron 12 34 23) 66.8)
+
+;ejerccio #3
+
+(define (triangulo-rec? a b c)
+  (cond
+    [(and (> a b) (> a c) ) (= (expt a 2)(+ (expt b 2)(expt c 2)))  ]
+    [(and (> b a) (> b c) ) (= (expt b 2)(+ (expt a 2)(expt c 2))) ]
+    [ display (= (expt c 2)(+ (expt b 2)(expt a 2))) ]
+    
+    )
+  )
+
+(test (triangulo-rec? 12 34 23) #f)
+(test (triangulo-rec? 10 34 11) #f)
+(test (triangulo-rec? 17 26 11) #f)
+(test (triangulo-rec? 5 8 11) #f)
+(test (triangulo-rec? 10 24 26) #t)
+;ejerccio #4
+(define (lista-num n)
+  
+  (string->number (string n)) )
+
+;5 - Inversión de números
+
+(define (invierte n)
+ (define (loop num reversa)
+    (cond
+      [(zero? num) reversa]
+      [(let ((ultimo (modulo num 10)))
+         (loop (/ (- num ultimo) 10) (+ (* reversa 10) ultimo)))]))
+   (loop n 0))
+
+(test (invierte 12345) 54321)
+(test (invierte 543) 345)
+(test (invierte 987) 789)
+(test (invierte 7421) 1247)
+(test (invierte 10101010) 1010101)
+
+
+
+
+;6 - Eliminar duplicados
+
+(define (elimina-dup l)
+  (cond
+    [(empty? l) '()]
+    [(empty? (cdr l)) (car l)]
+    [(equal? (first l) (second l)) (elimina-dup (cdr l))]
+    [else (cons (car l) (elimina-dup (cdr l)))]))
+
+
+;7 a
+(define (bin a)
+  (cond
+    [(< a 2) (number->string a)]
+    [else (string-append(bin(quotient a 2)) (number->string (modulo a 2)))]
+        ))
+
+
+(define (binarios l)
+  (cond
+    [(empty? l) '()]
+    [else (cons (bin (car l)) (binarios (cdr l)))]
+        ))
+;7 b
+(define (primo a b)
+  (cond
+    [(> 2 a) null]
+    [(> 2 b) a]
+    [(= (modulo a b) 0) null]
+    [else (primo a (- b 1))]
+    ))
+
+(define (primos l)
+  (cond
+    [(empty? l) null]
+    [else (remove '() (cons (primo (car l) (- (car l) 1)) (primos (cdr l))))]
+    ))
+;7 c
+(define (reversar l)
+  (cond
+    [(empty? l) '()]
+    [(foldr cons (list(car l)) (reversar(cdr l)))]
+    ))
+
+(define (reversal l)
+  (cond
+    [(empty? l) '()]
+    [(foldl cons '() l)]
+    ))
+
+;8a - Concatenación de dos listas
+(define (concatena l1 l2)
+  (cond ((empty? l1) l2)
+        ((empty? l2) l1)
+        (else (cons (car l1) (concatena (cdr l1) l2)))))
+
+;8b - Funcion a la lista
+(define (mapea f l)
+  (cond
+    [(empty? l) l]
+    [else (cons (f (car l)) (mapea f (cdr l)))]))
+
+;8c - Filtra
+(define (filtra p l)
+  (cond
+    [(empty? l) l]
+    [(p (car l)) (cons (car l) (filtra p (cdr l)))]
+    [(filtra p (cdr l))]))
+
+;8d Primeros n elementos
+(define (toma l n)
+  (define (aux li i j)
+    (cond
+    [(zero? j) i]
+    [else (aux (cdr li) (append i (list (car li))) (sub1 j))]))
+  (aux l empty n))
+
+;8e - quita
+(define (quita l n)
+  (define (aux li i j)
+    (cond
+      [(empty? li) '()]
+      [(< i j) (aux (cdr li) (add1 i) j)]
+      [else (append (list (first li)) (aux (cdr li) (add1 i) j))]))
+  (aux l 0 n))
+
+;9 a
+(letrec ([ mayor ( lambda ( n m )
+                   ( if ( < n m) m
+                        n))])
+  (mayor 1834 1729))
+
+
+;9 b
+
+(letrec ([ sumas ( lambda ( n )
+                   ( if ( < n 2) 1
+                        (+ n ( sumas (- n 1)))))])
+  (sumas 100))
---
+++ practica02/pract2.rkt
@@ -0,0 +1,106 @@
+#lang plai
+;; constructor de tipos 
+(define-type Figure
+  [triangulo (a number?) (b number?) (c number?)]
+  [cuadrado (a number?)] ; constructor 
+  [rectangulo (a number?) (b number?)]
+  [rombo (a number?) (D number?) (d number?)]
+  [paralelogramo (a number?) (b number?) (h number?)]
+  [circulo (D number?)]
+  [elipse (a number?) (b number?)])
+
+; saca el perimetro 
+(define (perimetro fig)
+  (cond
+    [(triangulo? fig) (+ (+ (triangulo-a fig) (triangulo-b fig)) (triangulo-c fig))]
+    [(cuadrado? fig) (* (cuadrado-a fig) 4)]
+    [(rectangulo? fig) (* (+ (rectangulo-a fig) (rectangulo-b fig)) 2)]
+    [(rombo? fig) (* (rombo-a) 4)]
+    [(paralelogramo? fig) (* (+ (paralelogramo-a fig) (paralelogramo-b fig)) 2)]
+    [(circulo? fig) (* pi (circulo-D fig))]
+    [(elipse? fig) (* (* 2 pi) (sqrt (/ (+ (* (elipse-a fig) (elipse-a fig)) (* (elipse-b fig) (elipse-b fig))) 2)))]))
+
+;area de el la figura
+
+(define (area fig)
+  (cond
+    [(triangulo? fig) (sqrt (* (* (* (/ (+ (triangulo-a fig) (+ (triangulo-b fig) (triangulo-c fig))) 2)
+                               (- (/ (+ (triangulo-a fig) (+ (triangulo-b fig) (triangulo-c fig))) 2) (triangulo-a fig)))
+                            (- (/ (+ (triangulo-a fig) (+ (triangulo-b fig) (triangulo-c fig))) 2) (triangulo-b fig)))
+                         (- (/ (+ (triangulo-a fig) (+ (triangulo-b fig) (triangulo-c fig))) 2) (triangulo-c fig))))]
+    [(cuadrado? fig) (* (cuadrado-a fig) (cuadrado-a))]
+    [(rectangulo? fig) (* (rectangulo-a fig) (rectangulo-b fig))]
+    [(rombo? fig) (/ (* (rombo-D fig) (rombo-d fig)) 2)]
+    [(paralelogramo? fig) (* (paralelogramo-h fig) (paralelogramo-b fig))];supongo b la base
+    [(circulo? fig) (* pi (expt (/ (circulo-D fig) 2) 2))]
+    [(elipse? fig) (* pi (* (elipse-a fig) (elipse-b fig)))]))
+;Un constructor (x) que representa una variable independiente x. Nótese que no recibe
+;parámetros.
+
+(define-type Funcion
+  [x]
+  [cte (n number?)]
+  [sum (f Funcion?) (g Funcion?)]
+  [mul (f Funcion?) (g Funcion?)]
+  [pot (b Funcion?) (n Funcion?)])
+
+(define (Funcion->string fun)
+  (cond
+    [(x? fun) (x)]
+    [(cte? fun) (cte-n fun)] ;Un constructor (cte n) para representar constantes como 2 o 1729, n es un número entero.
+    [(sum? fun) (~a "(" (Funcion->string (sum-f fun)) "+" (Funcion->string (sum-g fun)) ")")];Un constructor (sum f g) que representa una suma de funciones como x + 2, dónde f y g son funciones.
+    [(mul? fun)(~a "(" (Funcion->string (mul-f fun)) "*" (Funcion->string (mul-g fun)) ")")]
+    [(pot? fun) (~a "(" (Funcion->string (pot-b fun)) "^" (Funcion->string (pot-n fun)) ")")]))
+
+(define (evalua fun v)
+  (cond
+    [(cte? fun) (cte v)]
+    [(sum? fun) ]
+    [(mul? fun) ]
+    [(pot? fun) ]))
+
+;(define (deriva fun)
+;  (cond
+;    [(pot? fun) (mul (pot-n fun) (pot (pot-b fun) (- (pot-n fun) 1)))]))
+
+
+; car primero
+; cdr el ultimo
+;ejecccio #3 Pilas y Colas :
+
+(define-type Nodo
+  [vacio]
+  [nodo (any  elemento )(siguiente Nodo?)])
+
+(define elemento
+  [pila (n Nodo?)]
+  [mete-p (e Nodo?)(p Nodo?)]
+  [saca-p (p Nodo?)]
+  [mira-p (p Nodo?)]
+  
+  )
+;(define (pila n)
+ ; (cond
+ ;   [(n? Nodo)]
+ ;   [(me? )]
+;    
+   ; )
+  ;)
+
+ 
+;constructor pila n :
+
+
+;mete-p e p :
+
+;(saca-p p) : elimina un elemento en la pila , elimina al final 
+
+;contructor de (mira-p) p :
+
+
+
+
+
+
+
+
