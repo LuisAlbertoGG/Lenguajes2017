c22e4b263c4b23398bfb143172d89b6f3510bb4e
LuisAlbertoGG <pumasfutluis@ciencias.unam.mx>
2016-09-13 17:11:27 -0500

    Primeras 2 terminadas

--- practica02/pract2.rkt
+++ practica02/pract2.rkt
@@ -1,8 +1,8 @@
 #lang plai
-;; constructor de tipos 
+; constructor de tipos 
 (define-type Figure
   [triangulo (a number?) (b number?) (c number?)]
-  [cuadrado (a number?)] ; constructor 
+  [cuadrado (a number?)]
   [rectangulo (a number?) (b number?)]
   [rombo (a number?) (D number?) (d number?)]
   [paralelogramo (a number?) (b number?) (h number?)]
@@ -15,7 +15,7 @@
     [(triangulo? fig) (+ (+ (triangulo-a fig) (triangulo-b fig)) (triangulo-c fig))]
     [(cuadrado? fig) (* (cuadrado-a fig) 4)]
     [(rectangulo? fig) (* (+ (rectangulo-a fig) (rectangulo-b fig)) 2)]
-    [(rombo? fig) (* (rombo-a) 4)]
+    [(rombo? fig) (* (rombo-a fig) 4)]
     [(paralelogramo? fig) (* (+ (paralelogramo-a fig) (paralelogramo-b fig)) 2)]
     [(circulo? fig) (* pi (circulo-D fig))]
     [(elipse? fig) (* (* 2 pi) (sqrt (/ (+ (* (elipse-a fig) (elipse-a fig)) (* (elipse-b fig) (elipse-b fig))) 2)))]))
@@ -28,7 +28,7 @@
                                (- (/ (+ (triangulo-a fig) (+ (triangulo-b fig) (triangulo-c fig))) 2) (triangulo-a fig)))
                             (- (/ (+ (triangulo-a fig) (+ (triangulo-b fig) (triangulo-c fig))) 2) (triangulo-b fig)))
                          (- (/ (+ (triangulo-a fig) (+ (triangulo-b fig) (triangulo-c fig))) 2) (triangulo-c fig))))]
-    [(cuadrado? fig) (* (cuadrado-a fig) (cuadrado-a))]
+    [(cuadrado? fig) (* (cuadrado-a fig) (cuadrado-a fig))]
     [(rectangulo? fig) (* (rectangulo-a fig) (rectangulo-b fig))]
     [(rombo? fig) (/ (* (rombo-D fig) (rombo-d fig)) 2)]
     [(paralelogramo? fig) (* (paralelogramo-h fig) (paralelogramo-b fig))];supongo b la base
@@ -64,9 +64,13 @@
   (cond
     [(x? fun) (cte 1)]
     [(cte? fun) (cte 0)]
-    [(sum? fun) (sum (deriva (sum-f fun)) (deriva(sum-g fun)))]
-    [(mul? fun) (mul (pot-n fun) (pot (pot-b fun) (- (pot-n fun) 1)))]))
-
+    [(sum? fun) (sum (deriva (sum-f fun)) (deriva (sum-g fun)))]
+    [(mul? fun) (sum (mul (deriva (mul-f fun)) (mul-g fun)) (mul (deriva (mul-g fun)) (mul-f fun)))]
+    [(pot? fun) (mul (mul (pot-n fun) (pot (pot-b fun) (cte (- (funToNum (pot-n fun)) 1)))) (deriva (pot-b fun)))]))
+    
+(define (funToNum f)
+  (cond
+    [(cte? f) (cte-n f)]))
 ;(define (derivada f)
 ;   (cond
 ;    [(cte? f) (cte 0)]
